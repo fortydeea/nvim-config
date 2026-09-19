24705d32c679b4fd30892aac83ba2b10a7d0cbcf
fortydeea <40deea@gmail.com>
2026-09-18 17:16:43 -0700

    Fix folding
    Fix nvim-treesitter and other fold-related configs

--- init.lua
+++ init.lua
@@ -98,6 +98,23 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- Enough for treesitter folds to work
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+
+vim.opt.foldenable = true
+vim.opt.foldlevel = 99 -- start fully unfolded
+vim.opt.foldlevelstart = 99
+
+vim.opt.foldcolumn = '1'
+
+vim.opt.fillchars = {
+  fold = ' ',
+  foldopen = '',
+  foldclose = '',
+  foldsep = ' ',
+}
+
 -- Make line numbers default
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
@@ -930,8 +947,27 @@ require('lazy').setup({
     branch = 'main',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
     config = function()
-      -- ensure basic parser are installed
-      local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+      local parsers = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'cpp',
+        'python',
+        'java',
+        'go',
+        'rust',
+        'kotlin',
+        'javascript',
+        'tsx',
+      }
       require('nvim-treesitter').install(parsers)
 
       ---@param buf integer
